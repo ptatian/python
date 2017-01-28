25d19f03d33cf53c1feea68bdac3fcd4e1cf36b1
Peter Tatian <ptatian@urban.org>
2017-01-28 10:15:29 -0500

    Add print_prorgram(), other changes

--- digicomp/digicomp.py
+++ digicomp/digicomp.py
@@ -1,21 +1,41 @@
 import copy
 
 class cState:
+
     def __init__(self):
         self.labels = []
         self.values = []
 
+    def readout(self):
+        b = len(self.labels)
+        for a in range( b ):
+            print( self.labels[a], self.values[a] )
+        print( )
+
+    def set(self, n ):
+        self.values[n] = 1
+        self.readout()
+
+    def reset(self, n):
+        self.values[n] = 0
+        self.readout()
+        
 class cRod:
+
     def __init__(self,l,c):
         self.logic = l
         self.clock = c
 
 class cProgram:
+    
     def __init__(self):
+        self.title = ""
+        self.description = ""
         self.initstate = cState()
         self.rods = []
 
 def initstate():
+
     state = cState()
     b = len(program.initstate.labels)
     for a in range( b ):
@@ -52,11 +72,52 @@ def clock():
                 elif program.rods[i].clock[a] == 0:
                     newstate.values[a] = 0
     state.values = copy.deepcopy( newstate.values )
-    readout()
+    state.readout()
+
+def print_program():
+    bits = len(program.initstate.labels)
+    rods = len(program.rods)
+    stubsize = len(program.initstate.labels[0])
+
+    print( program.title )
+
+    ### Header
+    stub = stubsize * " "
+
+    print( stub, end='' )
+    for i in range(rods):
+        print( "   ", i + 1, end=' ' )
+    print()
+
+    print( stub + " ", end='' )
+    for i in range(rods):
+        print( " T F| ", end='' )
+    print()
+
+    for j in range(bits):
+        print( program.initstate.labels[j], end='  ' )
+        for i in range(rods):
+            if program.rods[i].logic[j] == None:
+                print( " | |", end='' )
+            elif program.rods[i].logic[j] == 0:
+                print( " |L|", end='' )
+            else:
+                print( "L| |", end='' )
+            if program.rods[i].clock[j] == None:
+                print( "  ", end='' )
+            else:
+                print( "C ", end='' )
+        print()
+
+    print()
+        
+    
+    
 
 program = cProgram()
 
-''' CHECKOUT
+''' CHECKOUT '''
+program.title = "Checkout"
 program.initstate.labels = [ "A", "B", "C" ]
 program.initstate.values = [ 0, 0, 0 ]
 program.rods.append( cRod( [ None, 0, None ], [ 0, None, None ] ) )
@@ -65,9 +126,10 @@ program.rods.append( cRod( [ None, None, 0 ], [ None, 0, None ] ) )
 program.rods.append( cRod( [ None, None, 1 ], [ None, 1, None ] ) )
 program.rods.append( cRod( [ 1, None, None ], [ None, None, 0 ] ) )
 program.rods.append( cRod( [ 0, None, None ], [ None, None, 1 ] ) )
-'''
+''' '''
 
-''' FINAL COUNT DOWN '''
+''' FINAL COUNT DOWN 
+program.title = "Final Count Down"
 program.initstate.labels = [ "A", "B", "C" ]
 program.initstate.values = [ 1, 1, 1 ]
 program.rods.append( cRod( [ 1, None, None ], [ 0, None, None ] ) )
@@ -76,13 +138,19 @@ program.rods.append( cRod( [ 0, 1, None ], [ None, 0, None ] ) )
 program.rods.append( cRod( [ 0, 0, None ], [ None, 1, None ] ) )
 program.rods.append( cRod( [ 0, 0, 1 ], [ None, None, 0 ] ) )
 program.rods.append( cRod( [ 0, 0, 0 ], [ None, None, 1 ] ) )
+'''
 
-print( "program:" )
-print( program.initstate.labels )
-print( program.initstate.values )
-print( program.rods[0].logic )
-print( program.rods[0].clock )
-print( "-------------" )
+''' AUTOMATIC ELEVATOR
+program.title = "Automatic Elevator"
+program.initstate.labels = [ "2nd floor", "1st floor", "Up/Down  " ]
+program.initstate.values = [ 0, 1, 0 ]
+program.rods.append( cRod( [ None, None, 0 ], [ 0, None, None ] ) )
+program.rods.append( cRod( [ None, None, 1 ], [ 1, None, None ] ) )
+program.rods.append( cRod( [ None, None, 1 ], [ None, 0, None ] ) )
+program.rods.append( cRod( [ None, None, 0 ], [ None, 1, None ] ) )
+'''
 
 state = initstate()
 readout()
+
+print_program()
